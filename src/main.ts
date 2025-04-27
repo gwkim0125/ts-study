8035517bc448d88b6fe16ca1ecb28c1875726645
gwkim0125 <gwkim0125@gmail.com>
2025-04-27 16:58:50 +0900

    m

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { input } from "./library/input";
 import * as fs from "node:fs";
-
+import { AuthUI } from "./view/authui";
 const appRun = async () => {
+  const authUI = new AuthUI();
   while (true) {
     let userEmail: string = "";
     let userPassword: string = "";
@@ -11,65 +12,23 @@ const appRun = async () => {
       "Press E to Sign in or Press R to Sign Up: ",
     );
     if (userSelect === "E") {
-      userEmail = await input("Please enter your email: ");
-      if (userEmail.includes("@") === false) {
-        console.log("Email is wrong.");
-        continue;
-      }
+      const user = await authUI.signIn();
+     if (user === null) {
+       console.log("Email or Password is Wrong");
+     }
+     else {
+       console.log(`Log In Successfull! ${user.getNickname()}`);
+       process.exit(0);
+     }
 
-      userPassword = await input("Please enter your password: ");
-      if (userPassword.length < 4) {
-        console.log("Your Password is too short.");
-        continue;
-      }
-
-      const userTextData = fs.readFileSync("user.txt").toString();
-      const userDataRows = userTextData.split("\n");
-      for (let i = 0; i < userDataRows.length; i++) {
-        const userProfile = userDataRows[i].split(", ");
-        if (userProfile[0] === userEmail && userProfile[1] === userPassword) {
-          console.log(`Welcome ${userProfile[2]}!`);
-          process.exit(0);
-        }
-      }
-
-      console.log("Email or Password is Wrong!");
     } else if (userSelect === "R") {
-      userEmail = await input("Please enter your email: ");
-      if (userEmail.includes("@") === false) {
-        console.log("Email is wrong.");
-        continue;
+      const signUpResult = await authUI.signUp();
+      if (signUpResult === false) {
+        console.log("Sign Up Failed, Please try again");
       }
-
-      userPassword = await input("Please enter your password: ");
-      if (userPassword.length < 4) {
-        console.log("Your Password is too short.");
-        continue;
+      else {
+        console.log("Sign Up Successfull!");
       }
-
-      userNickname = await input("Please enter your Nickname: ");
-
-      const userTextData = fs.readFileSync("user.txt").toString();
-      const userDataRows = userTextData.split("\n");
-      let isFoundEmail = false;
-      for (let i = 0; i < userDataRows.length; i++) {
-        const userProfile = userDataRows[i].split(", ");
-        if (userProfile[0] === userEmail) {
-          isFoundEmail = true;
-          console.log("This Email is already registered.");
-          break;
-        }
-      }
-
-      if (isFoundEmail === true) {
-        continue;
-      }
-
-      fs.appendFileSync(
-        "user.txt",
-        `${userEmail}, ${userPassword}, ${userNickname}\n`,
-      );
-      console.log("You are successfully registered.");
     } else {
       console.log("Wrong input!");
     }
---
+++ src/view/authui.ts
@@ -0,0 +1,55 @@
+import {input} from "../library/input";
+import fs from "node:fs";
+import {Database} from "../data/databse";
+
+export class AuthUI {
+    signIn = async () => {
+        const userEmail = await input("Please enter your email: ");
+        if (userEmail.includes("@") === false) {
+            console.log("Email is wrong.");
+            return null;
+        }
+
+        const userPassword = await input("Please enter your password: ");
+        if (userPassword.length < 4) {
+            console.log("Your Password is too short.");
+            return null;
+        }
+
+        const database = new Database();
+        return database.getUser(userEmail, userPassword);
+
+    }
+    signUp = async () => {
+        const userEmail = await input("Please enter your email: ");
+        if (userEmail.includes("@") === false) {
+            console.log("Email is wrong.");
+            return false;
+        }
+
+        const userPassword = await input("Please enter your password: ");
+        if (userPassword.length < 4) {
+            console.log("Your Password is too short.");
+            return false;
+        }
+
+        const userNickname = await input("Please enter your Nickname: ");
+        const userTextData = fs.readFileSync("user.txt").toString();
+        const userDataRows = userTextData.split("\n");
+        let isFoundEmail = false;
+        for (let i = 0; i < userDataRows.length; i++) {
+            const userProfile = userDataRows[i].split(", ");
+            if (userProfile[0] === userEmail) {
+                isFoundEmail = true;
+                console.log("User Email is already in use.");
+                return false;
+            }
+        }
+
+        fs.appendFileSync(
+            "user.txt",
+            `${userEmail}, ${userPassword}, ${userNickname}\n`,
+        );
+        return true;
+    }
+}
