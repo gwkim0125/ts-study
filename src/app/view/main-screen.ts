6182c887e571505cb1321e7ab286aca1e283314b
Wiz <sjkimwiz@gmail.com>
2025-07-20 11:24:49 +0900

    fix

--- src/app/view/main-screen.ts
+++ src/app/view/main-screen.ts
@@ -11,7 +11,7 @@ export class MainScreen {
     this.messageService = messageService;
   }
   public select = async () => {
-    return await input("[1] 메세지 작성, [2] 메세지 불러오기: ");
+    return await input("[1] 메세지 작성, [2] 메세지 불러오기, [3] 프로그램 종료하기: ");
   };
   public writeMessage = async (email: string) => {
     const isFormCorrect = await this.messageForm.receiveInput();
